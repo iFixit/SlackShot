060a576c131c8c759f1da86357a3971f2ae2e380
Evan W. Noronha <hello@evannoronha.com>
2018-07-01 15:55:09 -0700

    add eslint

--- src/listen.js
+++ src/listen.js
@@ -2,50 +2,58 @@ import { io } from 'socket.io-client';
 import { request } from 'request';
 import { jsdom } from 'jsdom';
 
-var socket = io("wss://realtime.dozuki.com");
+const socket = io('wss://realtime.dozuki.com');
 
-const ifixitSocket = io('wss://realtime.dozuki.com');
-const metaSocket = io('wss://realtime.dozuki.com');
+// const ifixitSocket = io('wss://realtime.dozuki.com');
+// const metaSocket = io('wss://realtime.dozuki.com');
 
 const iFixitSocketioRoom = 'ifixit 1101176 99ede35e3baded0cc07c65e78f3b8c2160f9ed0a';
 const MetaSocketioRoom = 'ifixit_meta 1101176 a99201d65c53a579ca0dca6d6af21a418a139405';
 
-socket.on('connect', function() {
+const onResponse = (error, response, body) => {
+   if (!error && response.statusCode === 200) {
+      console.log(body);
+   }
+};
+
+socket.on('connect', () => {
    socket.emit('subscribe', { room: iFixitSocketioRoom });
    socket.emit('subscribe', { room: MetaSocketioRoom });
 });
 
-socket.on('notification', function(notification) {
-   if (notification['event'] != 'notification') return;
-   console.log("Recieved notification:\n" + (notification["html"]));
-   console.log("Whole notification: \n " + JSON.stringify(notification));
-   
-   jsdom.env(notification["html"], ["http://code.jquery.com/jquery.js"], function (errors, window) {
-      const msg = window.$("div.notification-message").text().trim();
-      let lnk = window.$("a").attr("href");
+socket.on('notification', (notification) => {
+   if (notification.event !== 'notification') return;
+   console.log(`Recieved notification:\n + ${notification.html}`);
+   console.log(`Whole notification: \n + ${JSON.stringify(notification)}`);
 
-      var headers = { 'Content-type': 'application/json' };
+   jsdom.env(notification.html, ['http://code.jquery.com/jquery.js'], (errors, window) => {
+      const msg = window.$('div.notification-message').text().trim();
+      let lnk = window.$('a').attr('href');
 
-      if (lnk) {
-         lnk = "http://www.ifixit.com" + lnk;
-         var formattedString = "<" + lnk + "|" + msg + ">";
-      } else {
-         var formattedString = msg;
-      }
-      var dataString = '{"text":"' + formattedString + ' "}';
-
-      var options = {
-         url: 'https://hooks.slack.com/services/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',
-         method: 'POST',
-         headers: headers,
-         body: dataString
+      const headers = {
+         'Content-type': 'application/json',
       };
 
-      function callback(error, response, body) {
-         if (!error && response.statusCode == 200) {
-            console.log(body);
-         }
+      let formattedString;
+
+      if (lnk) {
+         lnk = `http://www.ifixit.com${lnk}`;
+         formattedString = `<${lnk}|${msg}>`;
+      } else {
+         formattedString = msg;
       }
-      request(options, callback);
+
+      const dataString = `{
+         "text": "${formattedString}"}
+      `;
+
+      const options = {
+         url: 'https://hooks.slack.com/services/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',
+         method: 'POST',
+         headers,
+         body: dataString,
+      };
+
+      request(options, onResponse);
    });
 });
