37065186b27dc8bb86ac881f59ec0c6b33e436d9
Evan Noronha <hello@evannoronha.com>
2018-07-02 09:37:46 -0700

    Remove webhook urls from example config

--- config.example.js
+++ config.example.js
@@ -1,17 +1,17 @@
 export default {
    'evan-ifixit': {
       socketioRoom: 'ifixit 1101176 hashgoeshere1234567890',
-      webhookUrl: 'https://hooks.slack.com/services/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',
+      webhookUrl: 'https://hooks.slack.com/services/slack/webhook/url',
       baseUrl: 'http://www.ifixit.com',
    },
    'evan-meta': {
       socketioRoom: 'ifixit_meta 1101176 hashgoeshere1234567890',
-      webhookUrl: 'https://hooks.slack.com/services/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',
+      webhookUrl: 'https://hooks.slack.com/services/slack/webhook/url',
       baseUrl: 'http://meta.ifixit.com',
    },
    'blake-ifixit': {
       socketioRoom: 'ifixit 1883877 hashgoeshere1234567890',
-      webhookUrl: 'https://hooks.slack.com/services/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',
+      webhookUrl: 'https://hooks.slack.com/services/slack/webhook/url',
       baseUrl: 'http://www.ifixit.com',
    },
 };
