07de3977093d7017dd48958cc6c00605cb13ff9d
Evan W. Noronha <hello@evannoronha.com>
2018-07-01 18:02:15 -0700

    Update webpack config to make it work

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-   entry: "./src/listen.js",
-   output: {
-      filename: "./bundle.js"
+   entry: './src/listen.js',
+   output: { filename: './bundle.js' },
+   target: 'node',
+   optimization: { minimize: false },
+   mode: 'development',
+   watch: true,
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['babel-preset-env'],
+               },
+            },
+         },
+      ],
    },
-   target : 'node',
-   optimization: {
-      minimize: false
-   },
-}
-
+};
