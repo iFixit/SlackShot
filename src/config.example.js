9fc1942645244017db2a6156b851c8abf34f1a17
Evan W. Noronha <hello@evannoronha.com>
2018-07-02 09:26:00 -0700

    Add a readme and extract the conf into a separate file

---
+++ src/config.example.js
@@ -0,0 +1,17 @@
+export default {
+   'evan-ifixit': {
+      socketioRoom: 'ifixit 1101176 hashgoeshere1234567890',
+      webhookUrl: 'https://hooks.slack.com/services/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',
+      baseUrl: 'http://www.ifixit.com',
+   },
+   'evan-meta': {
+      socketioRoom: 'ifixit_meta 1101176 hashgoeshere1234567890',
+      webhookUrl: 'https://hooks.slack.com/services/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',
+      baseUrl: 'http://meta.ifixit.com',
+   },
+   'blake-ifixit': {
+      socketioRoom: 'ifixit 1883877 hashgoeshere1234567890',
+      webhookUrl: 'https://hooks.slack.com/services/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',
+      baseUrl: 'http://www.ifixit.com',
+   },
+};
