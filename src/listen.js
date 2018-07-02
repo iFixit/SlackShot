b34665b24c813aa85309652d83d24e3e4d396e28
Evan W. Noronha <hello@evannoronha.com>
2018-07-01 22:16:18 -0700

    Do more fp stuff
    this commit breaks the slack relay functionality into its own module and separates notification deconstruction into its own function

--- src/listen.js
+++ src/listen.js
@@ -1,41 +1,22 @@
-import request from 'request';
 import io from 'socket.io-client';
 import cheerio from 'cheerio';
 
+import slackRelay from './slackRelay';
 // const ifixitSocket = io('wss://realtime.dozuki.com');
 // const metaSocket = io('wss://realtime.dozuki.com');
 
 const iFixitSocketioRoom = 'ifixit 1101176 99ede35e3baded0cc07c65e78f3b8c2160f9ed0a';
 const MetaSocketioRoom = 'ifixit_meta 1101176 a99201d65c53a579ca0dca6d6af21a418a139405';
 
-const onError = (err, res, body) => {
-   if (res !== 200) {
-      console.log('REQUEST RESULTS:', err, body);
-   }
-};
+const forwardToSlack = slackRelay('https://hooks.slack.com/services/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');
 
-const forwardToSlack = (url, text) => {
-   let formattedString;
+const destructureNotification = (html) => {
+   const notificationHtml = cheerio.load(html);
 
-   if (url) {
-      const completeUrl = `http://www.ifixit.com${url}`;
-      formattedString = `<${completeUrl}|${text}>`;
-   } else {
-      formattedString = text;
-   }
+   const text = notificationHtml('.notification-message').text().trim();
+   const link = notificationHtml('a').attr('href');
 
-
-   const uri = 'https://hooks.slack.com/services/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';
-   const headers = { 'Content-type': 'application/json' };
-   const body = `{"text":"  ${formattedString} "}`;
-
-   const options = {
-      uri,
-      headers,
-      body,
-   };
-
-   request.post(options, onError);
+   return { text, link };
 };
 
 const socket = io('wss://realtime.dozuki.com');
@@ -46,12 +27,8 @@ socket.on('connect', () => {
 });
 
 socket.on('notification', (notification) => {
-   if (notification.event !== 'notification') return;
-
-   const notificationHtml = cheerio.load(notification.html);
-
-   const text = notificationHtml('.notification-message').text().trim();
-   const link = notificationHtml('a').attr('href');
-
-   forwardToSlack(link, text);
+   if (notification.event === 'notification') {
+      const { text, link } = destructureNotification(notification.html);
+      forwardToSlack(link, text);
+   }
 });
---
+++ src/slackRelay.js
@@ -0,0 +1,32 @@
+import request from 'request';
+
+const onError = (err, res, body) => {
+   if (res !== 200) {
+      console.log('REQUEST RESULTS:', err, res, body);
+   }
+};
+
+export default function forwardToSlack(slackHook) {
+   return (url, text) => {
+      let formattedString;
+
+      if (url) {
+         const completeUrl = `http://www.ifixit.com${url}`;
+         formattedString = `<${completeUrl}|${text}>`;
+      } else {
+         formattedString = text;
+      }
+
+      const uri = slackHook;
+      const headers = { 'Content-type': 'application/json' };
+      const body = `{"text":"  ${formattedString} "}`;
+
+      const options = {
+         uri,
+         headers,
+         body,
+      };
+
+      request.post(options, onError);
+   };
+}
